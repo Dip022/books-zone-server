2e0ee60cd1804c8e4bb3beabfc71b7b87bf3c72d
Dipok Roy <roydipok022@gmail.com>
2022-05-04 16:23:49 +0600

    Mongodb setup and get all books success

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mlivg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const booksCollection = client.db("booksZone").collection("books");
+
+    //get all books
+    app.get("/books", async (req, res) => {
+      const query = {};
+      const cursor = booksCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // root server
 app.get("/", (req, res) => {
