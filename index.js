502450dc244bca966e97e6bda98190cf089d66ab
Dipok Roy <roydipok022@gmail.com>
2022-05-08 03:21:01 +0600

    heroku live link update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
@@ -8,6 +9,22 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).send({ message: "Unauthorized Access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mlivg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -20,6 +37,15 @@ async function run() {
     await client.connect();
     const booksCollection = client.db("booksZone").collection("books");
 
+    // post authoraizition
+    app.post("/singin", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+
     //get all books
     app.get("/books", async (req, res) => {
       const query = {};
@@ -29,12 +55,18 @@ async function run() {
     });
 
     //get all my item
-    app.get("/my-book/:email", async (req, res) => {
+    app.get("/my-book/:email", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const email = req.params.email;
-      const query = { email };
-      const cursor = booksCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
+
+      if (email === decodedEmail) {
+        const query = { email };
+        const cursor = booksCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "Forbidden Access" });
+      }
     });
 
     //get pegination item
@@ -85,7 +117,6 @@ async function run() {
     });
 
     // put stock book
-
     app.put("/stock-book/:id", async (req, res) => {
       const id = req.params.id;
       const updateDelivery = req.body;
